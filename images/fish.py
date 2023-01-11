fd4409a87c848126f4f515a0591d3afc247b860f
Winnie Jiang <winnie_jiang@aw.org>
2023-01-11 11:01:29 -0800

    fish

--- images/fish.py
+++ images/fish.py
@@ -5,24 +5,35 @@ class fish:
 
     def __init__(self, win):
 
+        
+        #empty list of all coords in grid
         listOfLocations = []
 
         
         x1 = 10
         y1 = 10
 
+        #loop that adds all coords in the list (append)
+
+        #for each x value
         while x1 <= 390:
             y1 = 10
 
+            #for each y value
             while y1 <= 390:
                 coords = (x1, y1)
                 y1 = y1 + 20
 
                 listOfLocations.append(coords)
             x1 = x1 + 20
-        
+
+        #sets its location to a random one within the list
         self.location = random.choice(listOfLocations)
 
+        self.x = self.location[0]
+        self.y = self.location[1]
+        
+        #draws fish with random location x and y ([0] and [1])
         self.body = Image(Point(self.location[0], self.location[1]), "fish.png")
         
         self.body.draw(win)
@@ -33,34 +44,48 @@ class fish:
 
     def randomMove(self, win):
 
+        #empty list of possible coords to move to
         newCoords = []
 
-        right = (self.location[0] + 20, self.location[1])
-        left = (self.location[0] - 20, self.location[1])
-        up = (self.location[0], self.location[1] - 20)
-        down = (self.location[0], self.location[1] + 20)
+        #possible movements
+        right = (20, 0)
+        left = (-20, 0)
+        up = (0, -20)
+        down = (0, 20)
 
+        #adds those movements' coords to list
         newCoords.append(right)
         newCoords.append(left)
         newCoords.append(up)
         newCoords.append(down)
 
-
+        #checks for if the location is outside of the grid or not
         for movement in newCoords:
-            if int(movement[0]) < 10 or int(movement[0]) > 390:
+            if int(self.x + movement[0]) < 10 or int(self.x + movement[0]) > 390:
                 newCoords.remove(movement)
-            if int(movement[1]) < 10 or int(movement[1]) > 390:
+            elif int(self.y + movement[1]) < 10 or int(self.y + movement[1]) > 390:
                 newCoords.remove(movement)
 
-        self.location = random.choice(newCoords)
-        self.body = Image(Point(self.location[0], self.location[1]), "fish.png")
-        self.body.draw(win)
+
+
+        #moves to a random location
+        newLocation = random.choice(newCoords)
+        self.body.move(newLocation[0], newLocation[1])
+
+        #updates the location
+        self.x = self.x + self.location[0]
+        self.y = self.y + self.location[1]
+        
 
     def location(self):
         self.location
 
     def programmedMove(self, win):
         k = 1
+        #ugh
+
+    def eaten(self):
+        self.undraw()
 
         
         
