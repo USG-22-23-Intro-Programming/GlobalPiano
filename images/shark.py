382c5a73cfa16c89946a9747436349896748c498
Winnie Jiang <winnie_jiang@aw.org>
2023-01-11 11:01:44 -0800

    shark class

--- images/shark.py
+++ images/shark.py
@@ -40,14 +40,167 @@ class shark:
     def draw(self, win):
         self.body.draw(win)
 
+    def location(self):
+        self.location
+
     def movement(self, win, f1, f2, f3):
 
+        #these determine the distance between the shark and each fish
         dis1 = abs(self.x - f1.location[0]) + abs(self.y - f1.location[1])
         dis2 = abs(self.x - f2.location[0]) + abs(self.y - f2.location[1])
         dis3 = abs(self.x - f3.location[0]) + abs(self.y - f3.location[1])
 
+        #possible movements
+        right = (20, 0)
+        left = (-20, 0)
+        up = (0, -20)
+        down = (0, 20)
 
-        #list of all possible locations of the shark
+        newCoords = []
+
+        #adds those movements' coords to list
+        newCoords.append(right)
+        newCoords.append(left)
+        newCoords.append(up)
+        newCoords.append(down)
+
+        #checks for if the location is outside of the grid or not
+        for movement in newCoords:
+            if int(self.x + movement[0]) < 10 or int(self.x + movement[0]) > 390:
+                newCoords.remove(movement)
+            if int(self.y + movement[1]) < 10 or int(self.y + movement[1]) > 390:
+                newCoords.remove(movement)
+
+        chase = 1
+
+        if self.location == f1.location:
+            chase = 2
+        if self.location == f2.location:
+            chase = 3
+
+        '''
+
+        #chase 1
+        #if the distance of shark1-fish1 is less than shark2-fish2 OR shark3-fish3
+        if dis1 < dis2 or dis1 < dis3:
+            chase = 1
+        if dis1 == dis2 or dis1 == dis3:
+            chase = 1
+            
+        #chase 2
+        if dis2 < dis1 and dis2 < dis3:
+            chase = 1
+        if dis2 == dis3:
+            chase = 1
+            
+        #chase 3
+        if dis3 < dis2 and dis3 < dis1:
+            chase = 1
+
+        '''
+
+
+        #x - 1
+        k1 = f1.location[0] - self.location[0]
+        #y - 1
+        k2 = f1.location[1] - self.location[1]
+
+        #x - 1
+        kk1 = f2.location[0] - self.location[0]
+        #y - 1
+        kk2 = f2.location[1] - self.location[1]
+
+        #x - 1
+        kkk1 = f3.location[0] - self.location[0]
+        #y - 1
+        kkk2 = f3.location[1] - self.location[1]
+        
+        #chase fish 1
+        if chase == 1:
+            #move towards fish 1
+            if k1 > 0:
+                self.body.move(right[0], right[1])
+                if k2 > 0:
+                    self.body.move(down[0], down[1])
+                elif k2 < 0:
+                    self.body.move(up[0], up[1])
+                else:
+                    idkidk = 1
+            elif k1 < 0:
+                self.body.move(left[0], left[1])
+                if k2 > 0:
+                    self.body.move(down[0], down[1])
+                elif k2 < 0:
+                    self.body.move(up[0], up[1])
+                else:
+                    idkidk = 2
+
+            else: #directly above / below. no side movements
+                for i in range(2):
+                    if k2 > 0:
+                        self.body.move(down[0], down[1])
+                    elif k2 < 0:
+                        self.body.move(up[0], up[1])
+
+
+        #chase fish 2
+        if chase == 2:
+            #move towards fish 2
+            if kk1 > 0:
+                self.body.move(right[0], right[1])
+                if kk2 > 0:
+                    self.body.move(down[0], down[1])
+                elif k2 < 0:
+                    self.body.move(up[0], up[1])
+                else:
+                    idkidk = 1
+            elif kk1 < 0:
+                self.body.move(left[0], left[1])
+                if kk2 > 0:
+                    self.body.move(down[0], down[1])
+                elif kk2 < 0:
+                    self.body.move(up[0], up[1])
+                else:
+                    idkidk = 2
+
+            else: #directly above / below. no side movements
+                for i in range(2):
+                    if kk2 > 0:
+                        self.body.move(down[0], down[1])
+                    elif kk2 < 0:
+                        self.body.move(up[0], up[1])
+                        
+        #chase fish 3
+        if chase == 3:
+            #move towards fish 3
+            if kkk1 > 0:
+                self.body.move(right[0], right[1])
+                if kkk2 > 0:
+                    self.body.move(down[0], down[1])
+                elif k2 < 0:
+                    self.body.move(up[0], up[1])
+                else:
+                    idkidk = 1
+            elif kkk1 < 0:
+                self.body.move(left[0], left[1])
+                if kkk2 > 0:
+                    self.body.move(down[0], down[1])
+                elif kkk2 < 0:
+                    self.body.move(up[0], up[1])
+                else:
+                    idkidk = 2
+
+            else: #directly above / below. no side movements
+                for i in range(2):
+                    if kkk2 > 0:
+                        self.body.move(down[0], down[1])
+                    elif kkk2 < 0:
+                        self.body.move(up[0], up[1])
+
+
+'''
+
+        #list of all possible movements of the shark
         
         sPosLoc = [(1, 0), (1, 1), (1, 2), (1, -1), (1, -2),
                               (2, 0), (2, -1), (2, 1),
@@ -70,24 +223,30 @@ class shark:
         
 
         #chase 1
+        #if the distance of shark1-fish1 is less than shark2-fish2 AND shark3-fish3
         if dis1 < dis2 and dis1 < dis3:
             chase = 1
         if dis1 == dis2 or dis1 == dis3:
             chase = 1
+            
         #chase 2
         if dis2 < dis1 and dis2 < dis3:
             chase = 1
         if dis2 == dis3:
             chase = 1
+            
         #chase 3
         if dis3 < dis2 and dis3 < dis1:
             chase = 1
 
+        
+        #chase fish 1
         if chase == 1:
             
-            possCoords = []
+            possCoords = [] #list of possible coordinates from the list of movements within the grid
             print(possCoords)
-            
+
+            #gets the distance between the shark and the fish and the movement that makes it closer
             for locations in sPosLoc:
                 xx1 = self.x + locations[0] *20
                 yy1 = self.y + locations[1] *20
@@ -95,13 +254,16 @@ class shark:
                 closeY = abs(yy1 - f1.location[1])
                 possCoords.append((closeX, closeY))
 
+            
+            #sorts the list by the closest - furthest
             possCoords.sort()
             print(possCoords)
-            
+
+            #gets the  closest
             closestCoord = possCoords[0]
-            xx = closestCoord[0]
+            xx = xx1
                 
-            yy = closestCoord[1]
+            yy = yy1
             
 
         #cannot both be greater than 2
@@ -110,8 +272,6 @@ class shark:
         self.body.draw(win)
         
 
-
-        '''
         
         dis1 = abs(self.location[0] - f1.location[0]) + abs(self.location[1] - f1.location[1])
         dis2 = abs(self.location[0] - f2.location[0]) + abs(self.location[1] - f2.location[1])
